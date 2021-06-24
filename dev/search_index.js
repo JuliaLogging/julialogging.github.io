704e6b0bf770f2efdba2573bb623208d5188de91
Documenter.jl <documenter@juliadocs.github.io>
2021-06-24 14:29:52 +0000

    build based on 315cb42

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JuliaLogging","page":"Home","title":"JuliaLogging","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the landing page of the JuliaLogging GitHub organization. JuliaLogging is an umbrella organization for logging-related packages and repositories of Julia programming language ecosystem.","category":"page"},{"location":"","page":"Home","title":"Home","text":"JuliaLogging is the home for the following packages and repositories:","category":"page"},{"location":"","page":"Home","title":"Home","text":"LoggingExtras – extensions to the Logging stdlib\nLokiLogger – log message sink for Grafana Loki","category":"page"}]
+}
